59fcf61ddc00beb9d6c14b403667e4f049e1b3e4
nicolas <nicolas.perrin83110@gmail.com>
2025-12-22 22:06:25 +0100

    initialize SpringBoot 4 application

---
+++ src/main/java/io/docpulse/docpulsebackend/DocpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package io.docpulse.docpulsebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocpulseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocpulseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/docpulse/docpulsebackend/DocpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.docpulse.docpulsebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocpulseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
